4f5bda61a08a4937b954e1007db7ab0012ea4ddc
Amit Kumar Pradhan <amitpradhankumar2009@gmail.com>
2026-01-21 15:24:39 +0530

    Implement authentication routes for register and login

---
+++ models/routes/auth.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = await User.create({ name, email, password: hash });
+  res.json(user);
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("User not found");
+
+  const match = await bcrypt.compare(req.body.password, user.password);
+  if (!match) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, "SECRET");
+  res.json({ token, user });
+});
+
+module.exports = router;
